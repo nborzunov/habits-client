d0d44e2acd688b6e2d4c7d07c1898ff138b07314
Nikolay Borzunov <forest3694@ya.ru>
2023-01-23 18:41:47 +0400

    Fixed deploy config

--- src/common/helpers/api.ts
+++ src/common/helpers/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.API_URL || 'http://localhost:8080/',
+    baseURL: import.meta.env.PROD
+        ? 'https://habits-server-production.up.railway.app/'
+        : 'http://localhost:8080/',
 });
 
 export default api;
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-
-export default ({mode}: any) => {
+export default ({ mode }: any) => {
     process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
 
     return defineConfig({
         plugins: [react(), tsconfigPaths()],
         server: {
             port: Number(process.env.PORT) || 3000,
-        }
+        },
     });
-}
+};
