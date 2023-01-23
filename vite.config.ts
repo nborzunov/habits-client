7b1e6a3c72c25791a07d2255c60d3230b16ccbc2
Nikolay Borzunov <forest3694@ya.ru>
2023-01-23 18:04:44 +0400

    Fixed deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import {defineConfig, loadEnv} from 'vite';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 
 
-
-export default ({ mode }) => {
+export default ({mode}) => {
     process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
 
     return defineConfig({
